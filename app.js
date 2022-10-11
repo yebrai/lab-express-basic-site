b41475cb8da9db4786d4b1492329a09d03c4da68
Borja Cabello Luna <borjacabello@outlook.es>
2022-10-11 17:42:40 +0200

    Preliminary app.js

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const port = 3000
 
 app.use(express.static("public"))
 
-app.get("/", (req, res) => {
-    
+app.get("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/home-page.html")
+    res.send("hola")
 })
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about-page.html")
+})
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/views/works-page.html")
+})
+
+app.get("/photo", (req, res) => {
+    res.sendFile(__dirname + "/views/photo-gallery-pages.html")
+})
+
+app.get("*", (req, res) => {
+    res.send("This page doesnt exist!")
+})
+
+app.listen(port, () => {})
